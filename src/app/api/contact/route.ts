f9ee72a1ba7caf61c3ad8fa3482bfe1f64b40c65
Zeeshan Asghar <mzeeshanff10@gmail.com>
2025-12-12 11:12:57 +0400

    replace Resend with nodemailer

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,22 +1,31 @@
 import { adminEmailTemplate, userEmailTemplate } from "@/lib/templates";
 import { FormType } from "@/schemas/contactForm";
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 export async function POST(req: Request) {
   try {
     const data: FormType = await req.json();
 
-    await resend.emails.send({
+    //
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST!,
+      port: Number(process.env.SMTP_PORT!),
+      secure: Number(process.env.SMTP_PORT) === 465,
+      auth: {
+        user: process.env.SMTP_USER!,
+        pass: process.env.SMTP_PASS!,
+      },
+    });
+
+    await transporter.sendMail({
       from: `MI MedCare <${process.env.CONTACT_EMAIL}>`,
       to: process.env.ADMIN_EMAIL!,
       subject: "New Contact Form Submission",
       html: adminEmailTemplate(data),
     });
 
-    await resend.emails.send({
+    await transporter.sendMail({
       from: `MI MedCare <${process.env.CONTACT_EMAIL}>`,
       to: data.email,
       subject: "We received your message",
@@ -24,8 +33,12 @@ export async function POST(req: Request) {
     });
 
     return NextResponse.json({ success: true });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ success: false, error }, { status: 500 });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error("Email error:", error);
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 }
+    );
   }
 }
