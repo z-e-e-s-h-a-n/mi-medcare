9cf700ec4a3a27197a02642e88db8edff3f1102e
Zeeshan Asghar <mzeeshanff10@gmail.com>
2026-03-10 01:55:49 +0400

    feat: update prebuild script

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ["@workspace/ui", "@workspace/sdk"],
   allowedDevOrigins: ["192.168.100.9"],
 
   images: {
