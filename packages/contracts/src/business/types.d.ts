d9b2290f87828cd7e99a2ac35365e9414892b52b
Zeeshan Asghar <mzeeshanff10@gmail.com>
2026-03-18 17:44:50 +0400

    Update business contract types

--- packages/contracts/src/business/types.d.ts
+++ packages/contracts/src/business/types.d.ts
@@ -12,16 +12,12 @@ export type BusinessProfileType = z.input<typeof businessProfileSchema>;
 export type BusinessPhone = z.input<typeof businessPhoneSchema>;
 export type BusinessAddress = z.input<typeof businessAddressSchema>;
 
-export interface BusinessProfileResponse
-  extends Omit<
-    Sanitize<BusinessProfile>,
-    "whatsapp" | "fax" | "phones" | "addresses"
-  > {
+export interface BusinessProfileResponse extends Sanitize<BusinessProfile> {
   whatsapp: BusinessPhone;
-  fax?: BusinessPhone[];
-  phones?: BusinessPhone[];
-  addresses?: BusinessAddress[];
-  logo?: MediaResponse;
-  favicon?: MediaResponse;
+  fax: BusinessPhone[];
+  phones: BusinessPhone[];
+  addresses: BusinessAddress[];
+  logo: MediaResponse;
+  favicon: MediaResponse;
   cover?: MediaResponse;
 }
